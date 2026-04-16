4e01237c8c46613de61c1b5031caca55f3e7fdc3
Azure Pipeline <azure-pipeline@local>
2026-04-16 15:26:28 +0700

    initial project

---
+++ backend/src/main/java/asset/management/last_resolve/LastResolveApplication.java
@@ -0,0 +1,13 @@
+package asset.management.last_resolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastResolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastResolveApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/asset/management/last_resolve/LastResolveApplicationTests.java
@@ -0,0 +1,13 @@
+package asset.management.last_resolve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastResolveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
